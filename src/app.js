19e2d4e59298a5a19306ccec7014e3da01723ddf
Ladino, Valeria <valeria.ladino@autozone.com>
2025-11-13 15:57:51 -0600

    Add Jest unit test for login

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const loginRoute = require('./routes/login');
+app.use('/login', loginRoute);
+
+module.exports = app;
